a0b9e8848ffd3b1a0e26988fb5a54d2583dfcf07
svensay <svensay@gmail.com>
2018-10-24 12:02:51 +0200

    Ajout des fichier 'Jeux de Test' et du fichier main.java qui a 2 fonction inf et eg de l'echaufement ainsi qu'un main avec les tests de ces même fonctions

---
+++ main.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+
+public class main{
+	public static boolean inf(byte cle1,byte cle2){
+		return cle1 < cle2;
+	}
+	public static boolean eg(byte cle1,byte cle2){
+		return cle1 == cle2;
+	}
+	public static void main(String[] args) {
+		System.out.println("1 inf 7: " + inf((byte)1,(byte)7));
+		System.out.println("1 inf 1: " + inf((byte)1,(byte)1));
+		System.out.println("8 inf 7: " + inf((byte)8,(byte)7));
+
+		System.out.println("1 eg 7: " + eg((byte)1,(byte)7));
+		System.out.println("1 eg 1: " + eg((byte)1,(byte)1));
+		System.out.println("8 eg 7: " + eg((byte)8,(byte)7));
+	}
+}
