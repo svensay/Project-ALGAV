df3bf85ec3dce3e5448a3e18adf991a3f650e86d
svensay <svensay@gmail.com>
2018-11-28 13:52:46 +0100

    Ajout des signature des primitive de fileBinomiale et tournoisBinomial

--- FileBinomiale.java
+++ FileBinomiale.java
@@ -4,4 +4,20 @@ public class FileBinomiale {
 	public FileBinomiale(List<TournoiBinomial> l){
 		this.l = l;
 	}
+
+	public boolean estVide(){
+		return true;
+	}
+
+	public TournoiBinomial minDeg(){
+		return null;
+	}
+
+	public FileBinomiale reste(){
+		return null;
+	}
+
+	public FileBinomiale AjoutMin(TournoiBinomial t){
+		return null;
+	}
 }
--- TournoiBinomial.java
+++ TournoiBinomial.java
@@ -1,13 +1,29 @@
 import java.util.*;
 public class TournoiBinomial{
-	byte v;
+	byte v; //cle128
 	List<TournoiBinomial> l;
 	public TournoiBinomial(byte v, List<TournoiBinomial> l){
 		this.v = v;
 		this.l =l;
 	}
 
-	boolean EstVide(){
+	public boolean EstVide(){
 		return l.isEmpty()||v==null;
+	}
+
+	public int Degre(){
+		return 0;
+	}
+
+	public TournoiBinomial Union2Tid(TournoiBinomial t){
+		return null;
+	}
+
+	public FileBinomiale Decapite(){
+		return null;
+	}
+
+	public FileBinomiale File(){
+		return null;
 	}
 }
