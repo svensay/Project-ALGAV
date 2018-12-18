5efc827660a582983908ce92649b4c20c57b44ed
Maxime <framagit@milletmaxime.net>
2018-12-18 13:18:33 +0100

    AVL équilibré

--- algav/avl/AVL.java
+++ algav/avl/AVL.java
@@ -5,42 +5,186 @@ public class AVL<T extends Comparable<T>> {
     private class AVLNoeud {
 
         private AVLNoeud filsGauche, filsDroit;
-        private final T cle;
+        private AVLNoeud parent;
+        private int profondeur;
+        private T cle;
 
-        public AVLNoeud(T cle)
+        public AVLNoeud(T cle, AVLNoeud parent, AVLNoeud gauche, AVLNoeud droit)
         {
             this.cle = cle;
-            this.filsDroit = this.filsGauche = null;
+            this.parent = parent;
+            this.filsDroit = droit;
+            this.filsGauche = gauche;
+
+            int max = -1;
+            if (gauche != null)
+            {
+                max = gauche.profondeur;
+            }
+            if (droit != null && droit.profondeur > max)
+            {
+                max = droit.profondeur;
+            }
+
+            this.profondeur = 1 + max;
         }
 
-        public boolean ajouter(T cle)
+        private void calculerProfondeur()
+        {
+            AVLNoeud noeud = this;
+            while (noeud != null)
+            {
+                int profondeurG = 0;
+                int profondeurD = 0;
+                if (noeud.filsGauche != null)
+                {
+                    profondeurG = noeud.filsGauche.profondeur;
+                }
+                if (noeud.filsDroit != null)
+                {
+                    profondeurD = noeud.filsDroit.profondeur;
+                }
+
+                noeud.profondeur = 1 + ((profondeurG > profondeurD) ? profondeurG : profondeurD);
+                noeud = noeud.parent;
+            }
+        }
+
+        private void equilibrer()
+        {
+            AVLNoeud n = this;
+            int facteur;
+            while (n != null)
+            {
+                facteur = ((n.filsGauche != null) ? n.filsGauche.profondeur : -1) - ((n.filsDroit != null) ? n.filsDroit.profondeur : -1);
+                if (facteur == 2)
+                {
+                    if (((n.filsGauche.filsGauche == null) ? -1 : n.filsGauche.filsGauche.profondeur) < ((n.filsGauche.filsDroit == null) ? -1 : n.filsGauche.filsDroit.profondeur))
+                    {
+                        n.filsGauche.rg();
+                    }
+                    n.rd();
+                    break;
+                }
+                else if (facteur == -2)
+                {
+                    if (((n.filsDroit.filsGauche == null) ? -1 : n.filsDroit.filsGauche.profondeur) > ((n.filsDroit.filsDroit == null) ? -1 : n.filsDroit.filsDroit.profondeur))
+                    {
+                        n.filsDroit.rd();
+                    }
+                    n.rg();
+                    break;
+                }
+                n = n.parent;
+            }
+        }
+
+        private void rg()
+        {
+            T copieCle = this.cle;
+
+            AVLNoeud A, C, E;
+            A = this.filsGauche;
+
+            C = this.filsDroit.filsGauche;
+            E = this.filsDroit.filsDroit;
+
+            this.cle = this.filsDroit.cle;
+            this.filsGauche = new AVLNoeud(copieCle, this, A, C);
+            this.filsDroit = E;
+            if (E != null)
+            {
+                E.parent = this;
+            }
+            if (A != null)
+            {
+                A.parent = this.filsGauche;
+            }
+            if (C != null)
+            {
+                C.parent = this.filsGauche;
+            }
+
+            if (this.filsGauche != null)
+            {
+                this.filsGauche.calculerProfondeur();
+            }
+            if (this.filsDroit != null)
+            {
+                this.filsDroit.calculerProfondeur();
+            }
+            this.calculerProfondeur();
+
+        }
+
+        private void rd()
+        {
+            T copieCle = this.cle;
+            AVLNoeud A, C, E;
+            E = this.filsDroit;
+            A = this.filsGauche.filsGauche;
+            C = this.filsGauche.filsDroit;
+
+            this.cle = this.filsGauche.cle;
+            this.filsDroit = new AVLNoeud(copieCle, this, C, E);
+            this.filsGauche = A;
+            if (E != null)
+            {
+                E.parent = this.filsDroit;
+            }
+            if (A != null)
+            {
+                A.parent = this;
+            }
+            if (C != null)
+            {
+                C.parent = this.filsDroit;
+            }
+
+            if (this.filsGauche != null)
+            {
+                this.filsGauche.calculerProfondeur();
+            }
+            if (this.filsDroit != null)
+            {
+                this.filsDroit.calculerProfondeur();
+            }
+            this.calculerProfondeur();
+
+        }
+
+        public AVLNoeud ajouter(T cle)
         {
             int comp = this.cle.compareTo(cle);
             if (comp > 0)
             {
                 if (this.filsGauche == null)
                 {
-                    this.filsGauche = new AVLNoeud(cle);
-                    return true;
+                    this.filsGauche = new AVLNoeud(cle, this, null, null);
+                    this.calculerProfondeur();
+                    this.equilibrer();
+                    return this.filsGauche;
                 }
                 else
                 {
-                    this.filsGauche.ajouter(cle);
+                    return this.filsGauche.ajouter(cle);
                 }
             }
             else if (comp < 0)
             {
                 if (this.filsDroit == null)
                 {
-                    this.filsDroit = new AVLNoeud(cle);
-                    return true;
+                    this.filsDroit = new AVLNoeud(cle, this, null, null);
+                    this.calculerProfondeur();
+                    this.equilibrer();
+                    return this.filsDroit;
                 }
                 else
                 {
-                    this.filsDroit.ajouter(cle);
+                    return this.filsDroit.ajouter(cle);
                 }
             }
-            return false;
+            return null;
         }
 
         public boolean rechercher(T cle)
@@ -79,42 +223,55 @@ public class AVL<T extends Comparable<T>> {
             return res;
         }
 
-        private void rg()
+        public String toString()
         {
-
-        }
-
-        private void rd()
-        {
-
-        }
-
-        private void rgd()
-        {
-
-        }
-
-        private void rdg()
-        {
-
+            String s = "{" + this.cle + ":g=";
+            if (this.filsGauche != null)
+            {
+                s += this.filsGauche;
+            }
+            else
+            {
+                s += "NULL";
+            }
+            s += ";d=";
+            if (this.filsDroit != null)
+            {
+                s += this.filsDroit;
+            }
+            else
+            {
+                s += "NULL";
+            }
+            return s += "}";
         }
     }
 
     private int taille;
     private AVLNoeud racine;
 
+    public AVL()
+    {
+        this.taille = 0;
+    }
+
     public AVL(T cle)
     {
         this.taille = 1;
-        this.racine = new AVLNoeud(cle);
+        this.racine = new AVLNoeud(cle, null, null, null);
     }
 
     public void ajouter(T cle)
     {
-        if (this.racine.ajouter(cle))
+        if (this.taille == 0)
         {
-            this.taille++;
+            this.racine = new AVLNoeud(cle, null, null, null);
         }
+        else
+        {
+            this.racine.ajouter(cle);
+        }
+        this.taille++;
     }
 
     public boolean rechercher(T cle)
@@ -125,6 +282,11 @@ public class AVL<T extends Comparable<T>> {
     public int getTaille()
     {
         return this.taille;
+    }
+
+    public String getArbreString()
+    {
+        return this.racine.toString();
     }
 
     @Override
